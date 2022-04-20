8a6a66c2549bd67005933f08c21aca32d036021a
Romain Devaux <r.devaux@icloud.com>
2022-04-20 15:33:16 +0200

    Blocage ecran mode portrait

---
+++ public/javascript/base.js
@@ -0,0 +1,7 @@
+// Fonction retour page précédente au click sur le chevron
+function retour() {
+    window.history.back();
+}
+
+// Bloque l'orientation de l'écran en mode portrait
+screen.lockOrientation("portrait");
